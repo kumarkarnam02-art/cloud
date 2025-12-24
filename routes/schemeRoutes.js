f4cc476f9553934553dc6dc0204a4838a89c08a1
kumarkarnam02-art <kumarkarnam02@gmail.com>
2025-12-24 11:39:01 +0530

    Update schemeRoutes.js

--- routes/schemeRoutes.js
+++ routes/schemeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Scheme = require("../models/Scheme");
-const auth = require("auth");
+const auth = require("./auth");
 
 const router = express.Router();
 
